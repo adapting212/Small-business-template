f300b910dc7f30e4e9a52a2470aac5d5ae690f18
adapting212 <pritom.dutta@hotmail.com>
2024-05-30 19:01:09 -0400

    re structured some components

--- frontend/src/navBar/Navbar.jsx
+++ frontend/src/navBar/Navbar.jsx
@@ -1,40 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-import logo from '../assets/logo.svg'
-import "../styles/navbar.scss";
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+import '../styles/navbar.scss';
 
 const Navbar = ({ dispatch, state }) => {
- 
-  return (
-    <>
-      <nav className="nav">
-        <div className="nav-logo">
-          <img src={logo} alt="Logo" className="nav-image" width="210" height="29" aria-hidden="true" decoding="async"/>
-        </div>
-        <div className="nav-menu">
-          <NavLink to="/" className="nav-link" activeClassName="active">
-            Home
-            </NavLink>
-          <NavLink to="/about" className="nav-link" activeClassName="active">
-            About
-            </NavLink>
-          <NavLink to="/" className="nav-link" activeClassName="active">
-            Aesthetics
-            </NavLink>
-          <NavLink to="/" className="nav-link" activeClassName="active">
-            Weight Loss
-            </NavLink>
-          <NavLink to="/" className="nav-link" activeClassName="active">
-            FAQ
-            </NavLink>
-          <NavLink to="/" className="contact-us" activeClassName="active">
-            Contact Us
-          </NavLink>
-         
+  const [isExpanded, setIsExpanded] = useState(false);
 
-        </div>
-      </nav>
-    </>
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsExpanded(window.scrollY === 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${isExpanded ? 'expanded' : ''}`}>
+      <div className={`nav-logo ${isExpanded ? 'expanded' : ''}`}>
+        <img src={logo} alt="Logo" className="nav-image" width="210" height="29" aria-hidden="true" decoding="async" />
+      </div>
+      <div className={`nav-menu ${isExpanded ? 'expanded' : ''}`}>
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/about" className="nav-link" activeClassName="active">
+          About
+        </NavLink>
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          Aesthetics
+        </NavLink>
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          Weight Loss
+        </NavLink>
+        <NavLink to="/" className="nav-link" activeClassName="active">
+          FAQ
+        </NavLink>
+        <NavLink to="/" className="contact-us" activeClassName="active">
+          Contact Us
+        </NavLink>
+      </div>
+    </nav>
   );
 };
 
